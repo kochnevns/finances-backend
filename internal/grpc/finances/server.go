7aa8bb79e9093f03df97b01745aa5c21e48ec3c5
kochnevns <nikitulko@gmail.ocm>
2024-07-02 22:45:06 +0300

    year

--- internal/grpc/finances/server.go
+++ internal/grpc/finances/server.go
@@ -93,7 +93,7 @@ func (s *serverAPI) MassiveReport(ctx context.Context, in *financesgrpcsrv.Massi
 	nowMonth := int(time.Now().Month())
 	nowYear := time.Now().Year()
 
-	for i := nowMonth - 3; i <= nowMonth; i++ {
+	for i := nowMonth - 12; i <= nowMonth; i++ {
 		month := i
 		year := nowYear
 		if i < 1 {
